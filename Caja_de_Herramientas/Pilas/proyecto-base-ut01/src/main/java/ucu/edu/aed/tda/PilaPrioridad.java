0764bac5859b46974ab3b90974509ae78f3eccd2
Lujan448 <LUJAN.UHALDE@correo.ucu.edu.uy>
2026-08-18 16:00:43 -0300

    agrego pilas apiladas

---
+++ Caja_de_Herramientas/Pilas/proyecto-base-ut01/src/main/java/ucu/edu/aed/tda/PilaPrioridad.java
@@ -0,0 +1,53 @@
+package ucu.edu.aed.tda;
+
+public class PilaPrioridad<T> extends ListaEnlazada<T> implements TDAPila<T> 
+{
+    private Nodo<T> tope;
+    private int cantidad;
+
+    public PilaPrioridad() 
+    {
+        tope = null;
+        cantidad = 0;
+    }
+
+    @Override
+    public void mete(T dato) 
+    {
+        Nodo<T> nuevo = new Nodo<>(dato);
+        nuevo.setSiguiente(tope); 
+        tope = nuevo;             
+        cantidad++;
+    }
+
+    @Override
+    public T saca() 
+    {
+        if (esVacia()) throw new RuntimeException("Pila vacía");
+        T dato = tope.getDato();
+        tope = tope.getSiguiente(); 
+        cantidad--;
+        return dato;
+    }
+
+    @Override
+    public T tope() 
+    {
+        if (esVacia()) throw new RuntimeException("Pila vacía");
+        {
+            return tope.getDato();
+        }
+    }
+
+    @Override
+    public boolean esVacia() 
+    {
+        return tope == null;
+    }
+
+    @Override
+    public int tamano() 
+    {
+        return cantidad;
+    }
+}
