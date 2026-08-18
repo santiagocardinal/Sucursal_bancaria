ced02d1652bb3381ccc7d005dec26fda5f5b4f33
Lujan448 <LUJAN.UHALDE@correo.ucu.edu.uy>
2026-08-18 14:15:41 -0300

    agrego arreglos a la clase nodo

--- Caja_de_Herramientas/Listas/proyecto-base-ut01/src/main/java/ucu/edu/aed/tda/Nodo.java
+++ Caja_de_Herramientas/Listas/proyecto-base-ut01/src/main/java/ucu/edu/aed/tda/Nodo.java
@@ -4,10 +4,12 @@ public class Nodo <T> {
 
     private T dato;
     private Nodo<T> siguiente;
+    private Nodo<T> anterior;
 
     public Nodo (T dato){
         this.dato = dato;
         this.siguiente = null;
+        this.anterior = null;
     }
 
     public T getDato(){
@@ -23,6 +25,14 @@ public class Nodo <T> {
 
     public void setSiguiente(Nodo<T> siguiente){
         this.siguiente = siguiente;
+    }
+
+    public Nodo<T> getAnterior(){
+        return this.anterior;
+    }
+
+    public void setAnterior(Nodo<T> anterior){
+        this.anterior = anterior;
     }
 }
 
