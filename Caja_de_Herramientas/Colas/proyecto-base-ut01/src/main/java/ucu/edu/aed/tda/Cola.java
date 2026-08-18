71806abb71e4d9bc47781919fc05f1dcd28a0c69
Lujan448 <LUJAN.UHALDE@correo.ucu.edu.uy>
2026-08-18 15:42:45 -0300

    agrego cola

---
+++ Caja_de_Herramientas/Colas/proyecto-base-ut01/src/main/java/ucu/edu/aed/tda/Cola.java
@@ -0,0 +1,81 @@
+package ucu.edu.aed.tda;
+
+import java.util.NoSuchElementException;
+import java.util.Comparator;
+import java.util.function.Predicate;
+import ucu.edu.aed.tda.TDALista;
+
+public class Cola<T> extends ListaEnlazada<T> implements TDACola<T> 
+{
+    //los 3 metodos siguientes son metodos propias de las colas
+    @Override
+    public T frente()
+    {
+        if (esVacio()) 
+        {
+            throw new NoSuchElementException("la cola esta vacia");
+        }
+        return obtener(0);
+    }
+
+    @Override
+    public boolean poneEnCola(T dato)
+    {
+        agregar(dato);
+        return true;
+    }
+
+    @Override
+    public T quitaDeCola()
+    {
+        if (esVacio()) 
+        {
+            throw new NoSuchElementException("la pila esta vacia");
+        }
+        return remover(0);
+
+    }
+
+    //estos metodos son los metodos que se heredaron de TDALista
+    @Override
+    public boolean contiene(T elem) 
+    { 
+        return contiene(elem); 
+    }
+
+    @Override
+    public int indiceDe(T elem) 
+    { 
+        return indiceDe(elem); 
+    }
+
+    @Override
+    public T buscar(Predicate<T> criterio) 
+    { 
+        return buscar(criterio); 
+    }
+
+    @Override
+    public TDALista<T> ordenar(Comparator<T> comparator) 
+    { 
+        return ordenar(comparator); 
+    }
+
+    @Override
+    public int tamano() 
+    { 
+        return super.tamano(); 
+    }
+
+    @Override
+    public boolean esVacio() 
+    { 
+        return super.esVacio(); 
+    }
+
+    @Override
+    public void vaciar() 
+    { 
+        super.vaciar(); 
+    }
+}
