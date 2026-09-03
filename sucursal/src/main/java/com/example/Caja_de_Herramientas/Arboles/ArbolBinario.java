dc87b3d955cdf835d96e2914c3de4faa5a9fa3ec
Lujan448 <LUJAN.UHALDE@correo.ucu.edu.uy>
2026-09-03 14:21:28 -0300

    agrego arbol binario y elelemento

---
+++ sucursal/src/main/java/com/example/Caja_de_Herramientas/Arboles/ArbolBinario.java
@@ -0,0 +1,206 @@
+package com.example.Caja_de_Herramientas.Arboles;
+import java.util.function.Consumer;
+
+public class ArbolBinario extends Comparable<T> implements TDAArbolBinario<T> 
+{
+    private TDAElemento<T> raiz;
+    private int contador;
+    private int contadorBusquedas;
+
+    public ArbolBinario() 
+    {
+        this.raiz = null;
+        this.contador = 0;
+        this.contadorBusquedas = 0;
+    }
+
+    @Override
+    public boolean esVacio()
+    {
+        return raiz == null;
+    }
+
+    @Override
+    public TDAElemento<T> obtenerRaiz()
+    {
+        return raiz;
+    }
+
+    @Override
+    public boolean insertar(T dato)
+    {
+        boolean insertado;
+
+        if (esVacio())
+        {
+            raiz = new Elemento<>((T) dato);
+            insertado = true;
+        }
+        else
+        {
+            insertado = raiz.insertar(dato);
+        }
+
+        if (insertado)
+        {
+            contador++;
+        }
+
+        System.out.println("Contador: " + contador);
+        return insertado;
+    }
+
+    @Override
+    public T buscar(Comparable<T> criterioBusqueda)
+    {
+        contadorBusquedas++;
+
+        if (esVacio())
+        {
+            return null;
+        }
+        TDAElemento<T> buscado = raiz.buscar(criterioBusqueda);
+        if (buscado == null)
+        {
+            return null;
+        }
+        else
+        {
+            return buscado.getDato();
+        }
+    }
+
+    @Override
+    public boolean eliminar(Comparable<T> criterioBusqueda)
+    {
+        if (esVacio())
+        {
+            return false;
+        }
+
+        if (criterioBusqueda.compareTo(raiz.getDato()) == 0)
+        {
+            if (raiz.getHijoIzquierdo() == null)
+            {
+                raiz = raiz.getHijoDerecho();
+            }
+            else if (raiz.getHijoDerecho() == null)
+            {
+                raiz = raiz.getHijoIzquierdo();
+            }
+            else
+            {
+                TDAElemento<T> sucesor = raiz.getHijoDerecho();
+                while (sucesor.getHijoIzquierdo() != null)
+                {
+                    sucesor = sucesor.getHijoIzquierdo();
+                }
+                T datoSucesor = sucesor.getDato();
+                raiz.eliminar((Comparable<T>) datoSucesor);
+                raiz.setDato(datoSucesor);
+            }
+            return true;
+        }
+        return raiz.eliminar(criterioBusqueda) != null;
+    }
+
+    @Override
+    public void preOrder(Consumer<T> consumidor)
+    {
+        if (!esVacio())
+        {
+            raiz.preOrder(nodo -> consumidor.accept(nodo.getDato()));
+        }
+    }
+
+    @Override
+    public void inOrder(Consumer<T> consumidor)
+    {
+        if (!esVacio())
+        {
+            raiz.inOrder(nodo -> consumidor.accept(nodo.getDato()));
+        }
+    }
+
+    @Override
+    public void postOrder(Consumer<T> consumidor)
+    {
+        if (!esVacio())
+        {
+            raiz.postOrder(nodo -> consumidor.accept(nodo.getDato()));
+        }
+    }
+
+    @Override
+    public int cantidadNodos()
+    {
+        if (esVacio())
+        {
+            return 0;
+        }
+        return raiz.cantidadNodos();
+    }
+
+    @Override
+    public int cantidadHojas()
+    {
+        if (esVacio())
+        {
+            return 0;
+        }
+        return raiz.cantidadHojas();
+    }
+
+    @Override
+    public int cantidadNodosInternos()
+    {
+        if (esVacio())
+        {
+            return 0;
+        }
+        return raiz.cantidadNodosInternos();
+    }
+
+    public int getContador() 
+    {
+        return contador;
+    }
+
+    public int getContadorBusquedas() 
+    {
+        return contadorBusquedas;
+    }
+
+    public String preOrderString()
+    {
+        if(esVacio())
+        {
+            return "el arbol esta vacio";
+        }
+        StringBuilder string = new StringBuilder();
+        raiz.preOrder(nodo -> { string.append(nodo.getDato()).append(" "); });
+        return string.toString();
+    }
+
+    public String postOrderString()
+    {
+        if(esVacio())
+        {
+            return "el arbol esta vacio";
+        }
+        StringBuilder string = new StringBuilder();
+        raiz.postOrder(nodo -> { string.append(nodo.getDato()).append(" "); });
+        return string.toString();
+    }
+
+    public String inOrderString()
+    {
+        if(esVacio())
+        {
+            return "el arbol esta vacio";
+        }
+        StringBuilder string = new StringBuilder();
+        raiz.inOrder(nodo -> { string.append(nodo.getDato()).append(" "); });
+        return string.toString();
+    }
+}
---
+++ sucursal/src/main/java/com/example/Caja_de_Herramientas/Arboles/Elemento.java
@@ -0,0 +1,333 @@
+package com.example.Caja_de_Herramientas.Arboles;
+
+public class Elemento implements TDAElemento<T> 
+{
+    private T dato;
+    private TDAElemento<T> hijoIzquierdo;
+    private TDAElemento<T> hijoDerecho;
+
+    public Elemento(T dato) 
+    {
+        this.dato = dato;
+        this.hijoIzquierdo = null;
+        this.hijoDerecho = null;
+    }
+
+    @Override
+    public void setHijoIzquierdo(TDAElemento<T> hijoIzquierdo) 
+    {
+        this.hijoIzquierdo = hijoIzquierdo;
+    }
+
+    @Override
+    public void setHijoDerecho(TDAElemento<T> hijoDerecho) 
+    {
+        this.hijoDerecho = hijoDerecho;
+    }
+
+    @Override
+    public TDAElemento<T> getHijoIzquierdo() 
+    {
+        return hijoIzquierdo;
+    }
+
+    @Override
+    public TDAElemento<T> getHijoDerecho() 
+    {
+        return hijoDerecho;
+    }
+
+    @Override
+    public void setDato(T dato) 
+    {
+        this.dato = dato;
+    }
+
+    @Override
+    public T getDato() 
+    {
+        return dato;
+    }
+
+    @Override
+    public boolean esHoja()
+    {
+        return (getHijoIzquierdo() == null && getHijoDerecho() == null);
+    }
+
+    @Override
+    public boolean insertar(Comparable<T> nuevoDato)
+    {
+        int compare = nuevoDato.compareTo(this.dato);
+        if(compare == 0)
+        {
+            return false;
+        }
+        else if(compare < 0 )
+        {
+            if(hijoIzquierdo == null)
+            {
+                hijoIzquierdo = new Elemento<>((T) nuevoDato);
+                return true;
+            }
+            else
+            {
+                return hijoIzquierdo.insertar(nuevoDato);
+            }
+        }
+        else 
+        {
+            if (hijoDerecho == null)
+            {
+                hijoDerecho = new Elemento<>((T) nuevoDato);
+                return true;
+            }
+            else
+            {
+                return hijoDerecho.insertar(nuevoDato);
+            }
+        }     
+    } 
+
+    @Override
+    public TDAElemento<T> buscar(Comparable<T> criterioBusqueda)
+    {
+        int compare = criterioBusqueda.compareTo(this.dato);
+        if(compare == 0)
+        {
+            return this;
+        }
+        else if(compare < 0 )
+        {
+            if(hijoIzquierdo == null)
+            {
+                return null;
+            }
+            else
+            {
+                return hijoIzquierdo.buscar(criterioBusqueda);
+            }
+        }
+        else 
+        {
+            if (hijoDerecho == null)
+            {
+                return null;
+            }
+            else
+            {
+                return hijoDerecho.buscar(criterioBusqueda);
+            }
+        }    
+    }
+    
+    @Override
+    public void preOrder(Consumer<TDAElemento<T>> consumidor)
+    {
+        consumidor.accept(this);
+        if(hijoIzquierdo != null)
+        {
+            hijoIzquierdo.preOrder(consumidor);
+        }
+        if (hijoDerecho != null)
+        {
+            hijoDerecho.preOrder(consumidor);
+        }
+    }
+
+    @Override
+    public void postOrder(Consumer<TDAElemento<T>> consumidor)
+    {
+        if(hijoIzquierdo != null)
+        {
+            hijoIzquierdo.postOrder(consumidor);
+        }
+        if (hijoDerecho != null)
+        {
+            hijoDerecho.postOrder(consumidor);
+        }
+        consumidor.accept(this);
+    }
+
+    @Override
+    public void inOrder(Consumer<TDAElemento<T>> consumidor)
+    {
+        if(hijoIzquierdo != null)
+        {
+            hijoIzquierdo.inOrder(consumidor);
+        }
+        consumidor.accept(this);
+        if (hijoDerecho != null)
+        {
+            hijoDerecho.inOrder(consumidor);
+        }
+    }
+
+    @Override
+    public int cantidadHojas()
+    {
+        if (esHoja())
+        {
+            return 1;
+        }
+
+        int izq = 0;
+        if (hijoIzquierdo != null)
+        {
+            izq = hijoIzquierdo.cantidadHojas();
+        }
+
+        int der = 0;
+        if (hijoDerecho != null)
+        {
+            der = hijoDerecho.cantidadHojas();
+        }
+
+        return izq + der;
+    }
+
+    @Override
+    public int cantidadNodosInternos()
+    {
+        if (esHoja())
+        {
+            return 0;
+        }
+
+        int izq = 0;
+        if (hijoIzquierdo != null)
+        {
+            izq = hijoIzquierdo.cantidadNodosInternos();
+        }
+
+        int der = 0;
+        if (hijoDerecho != null)
+        {
+            der = hijoDerecho.cantidadNodosInternos();
+        }
+
+        return 1 + izq + der;
+    }
+
+    @Override
+    public int cantidadNodos()
+    {
+        int izq = 0;
+        if (hijoIzquierdo != null)
+        {
+            izq = hijoIzquierdo.cantidadNodos();
+        }
+
+        int der = 0;
+        if (hijoDerecho != null)
+        {
+            der = hijoDerecho.cantidadNodos();
+        }
+
+        return 1 + izq + der;
+    }
+
+    @Override
+    public int altura()
+    {
+        int alturaIzq = -1;
+        if (hijoIzquierdo != null)
+        {
+            alturaIzq = hijoIzquierdo.altura();
+        }
+
+        int alturaDer = -1;
+        if (hijoDerecho != null)
+        {
+            alturaDer = hijoDerecho.altura();
+        }
+
+        return 1 + Math.max(alturaIzq, alturaDer);
+    }
+
+    @Override
+    public int obtenerNivel(Comparable<T> criterioBusqueda)
+    {
+        int compare = criterioBusqueda.compareTo(this.dato);
+        if (compare == 0)
+        {
+            return 0;
+        }
+        if (compare < 0)
+        {
+            if (hijoIzquierdo == null)
+            {
+                return -1;
+            }
+            int nivel = hijoIzquierdo.obtenerNivel(criterioBusqueda);
+            if (nivel == -1)
+            {
+                return -1;
+            }
+            return nivel + 1;
+        }
+        else
+        {
+            if (hijoDerecho == null)
+            {
+                return -1;
+            }
+            int nivel = hijoDerecho.obtenerNivel(criterioBusqueda);
+            if (nivel == -1)
+            {
+                return -1;
+            }
+            return nivel + 1;
+        }
+    }
+
+    @Override
+    public TDAElemento<T> eliminar(Comparable<T> criterioBusqueda)
+    {
+        if (hijoIzquierdo != null && criterioBusqueda.compareTo(hijoIzquierdo.getDato()) == 0)
+        {
+            TDAElemento<T> eliminado = hijoIzquierdo;
+            hijoIzquierdo = reemplazar(hijoIzquierdo);
+            return eliminado;
+        }
+        if (hijoDerecho != null && criterioBusqueda.compareTo(hijoDerecho.getDato()) == 0)
+        {
+            TDAElemento<T> eliminado = hijoDerecho;
+            hijoDerecho = reemplazar(hijoDerecho);
+            return eliminado;
+        }
+
+        int compare = criterioBusqueda.compareTo(this.dato);
+        if (compare < 0 && hijoIzquierdo != null)
+        {
+            return hijoIzquierdo.eliminar(criterioBusqueda);
+        }
+        if (compare > 0 && hijoDerecho != null)
+        {
+            return hijoDerecho.eliminar(criterioBusqueda);
+        }
+        return null;
+    }
+
+    private TDAElemento<T> reemplazar(TDAElemento<T> nodo)
+    {
+        if (nodo.getHijoIzquierdo() == null)
+        {
+            return nodo.getHijoDerecho();
+        }
+        if (nodo.getHijoDerecho() == null)
+        {
+            return nodo.getHijoIzquierdo();
+        }
+
+        TDAElemento<T> sucesor = nodo.getHijoDerecho();
+        while (sucesor.getHijoIzquierdo() != null)
+        {
+            sucesor = sucesor.getHijoIzquierdo();
+        }
+        T datoSucesor = sucesor.getDato();
+        nodo.eliminar((Comparable<T>) datoSucesor);
+        nodo.setDato(datoSucesor);
+        return nodo;
+    }
+}
